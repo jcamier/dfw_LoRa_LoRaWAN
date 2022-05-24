c7c82c232874dc6671901ffe2a3bef38e76866f5
jcamier <jcamier@gmail.com>
2022-05-24 06:50:27 -0500

    add more dbm to readme and start mqtt code

--- rfm9x_rpi_lorawan.py
+++ rfm9x_rpi_lorawan.py
@@ -1,21 +1,44 @@
 # GitHub repo: https://github.com/adafruit/Adafruit_CircuitPython_RFM9x
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+# Author: Jacques Camier
 
-# Simple demo of sending and receiving data with the RFM95 LoRa radio.
-# Author: Tony DiCola
+import os
+import sys
+import logging
+import json
+import csv
+import random
+from datetime import datetime
+
 import board
 import busio
 import digitalio
-
 import adafruit_rfm9x
+import paho.mqtt.client as mqtt
 
+
+# Procedure to get the USER, PASSWORD, PUBLIC_TLS_ADDRESS and PUBLIC_TLS_ADDRESS_PORT:
+# 1. Login to The Things Stack Community Edition console
+#    https://console.cloud.thethings.network/
+# 2. Select Go to applications
+# 3. Select your application
+# 4. On the left hand side menu, select Integrations | MQTT
+# 5. See Connection credentials
+# 6. For the password press button: Generate new API key
+#    Each time you press this button a new password is generated!
+#    The password looks like:
+#    NNSXS.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+USER = "XXXXXXXXXX@ttn"
+PASSWORD = "NNSXS.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+PUBLIC_TLS_ADDRESS = "nam1.cloud.thethings.network"
+PUBLIC_TLS_ADDRESS_PORT = 8883
+DEVICE_ID = "eui-XXXXXXXXXXXXXX"
+ALL_DEVICES = True
 
 # Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
+RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your country
 
-# Define pins connected to the chip, use these if wiring up the breakout according to the guide:
+# Define pins connected to the chip:
 CS = digitalio.DigitalInOut(board.CE1)
 RESET = digitalio.DigitalInOut(board.D25)
 
@@ -25,11 +48,8 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 # Initialize RFM radio
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
 
-# Note that the radio is configured in LoRa mode so you can't control sync
-# word, encryption, frequency deviation, or other settings!
 
-# You can however adjust the transmit power (in dB).  The default is 13 dB but
-# high power radios like the RFM95 can go up to 23 dB:
+# You can adjust the transmit power (in dB).  The default is 13 dB but
 rfm9x.tx_power = 23
 
 # Send a packet.  Note you can only send a packet up to 252 bytes in length.
@@ -39,31 +59,4 @@ rfm9x.tx_power = 23
 rfm9x.send(bytes("Hello world!\r\n", "utf-8"))
 print("Sent Hello World message!")
 
-# Wait to receive packets.  Note that this library can't receive data at a fast
-# rate, in fact it can only receive and process one 252 byte packet at a time.
-# This means you should only use this for low bandwidth scenarios, like sending
-# and receiving a single message at a time.
-print("Waiting for packets...")
 
-while True:
-    packet = rfm9x.receive()
-    # Optionally change the receive timeout from its default of 0.5 seconds:
-    # packet = rfm9x.receive(timeout=5.0)
-    # If no packet was received during the timeout then None is returned.
-    if packet is None:
-        # Packet has not been received
-        print("Received nothing! Listening again...")
-    else:
-        # Received a packet!
-        # Print out the raw bytes of the packet:
-        print("Received (raw bytes): {0}".format(packet))
-        # And decode to ASCII text and print it too.  Note that you always
-        # receive raw bytes and need to convert to a text format like ASCII
-        # if you intend to do string processing on your data.  Make sure the
-        # sending side is sending ASCII data before you try to decode!
-        packet_text = str(packet, "ascii")
-        print("Received (ASCII): {0}".format(packet_text))
-        # Also read the RSSI (signal strength) of the last received message and
-        # print it.
-        rssi = rfm9x.last_rssi
-        print("Received signal strength: {0} dB".format(rssi))
